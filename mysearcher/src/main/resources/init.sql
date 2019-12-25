7c03e358c1f45153588cec8f31356d314839bd09
frankzhiwen <frankzhiwen@163.com>
2019-12-25 20:22:12 +0800

    更新系统文件后，同步数据库信息

--- mysearcher/src/main/resources/init.sql
+++ mysearcher/src/main/resources/init.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS file_meta (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(50) NOT NULL,
     path VARCHAR(1000) NOT NULL,
+    is_directory BOOLEAN NOT NULL,
     size BIGINT NOT NULL,
     last_modified TIMESTAMP NOT NULL
 );
